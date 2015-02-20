c500c266c913beecb0704489c4ab75a9c46614c1
Greg Baker <ggbaker@sfu.ca>
2015-02-19 17:49:18 -0800

    working setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from distutils.core import setup
-from Cython.Build import cythonize
+from setuptools import setup
+try:
+    from Cython.Build import cythonize
+    have_cython = True
+except ImportError:
+    have_cython = False
+
+
+if have_cython:
+    ext_modules = cythonize('dynmodel/interpolatex.pyx')
+else:
+    ext_modules = []
 
 setup(name='Dynmodel',
       version='0.2',
       description='Helper for creating dynamic state variable models',
       author='Greg Baker',
       author_email='ggbaker@sfu.ca',
+      license='GPL2',
       packages=['dynmodel'],
-      ext_modules = cythonize('dynmodel/interpolatex.pyx'),
+      install_requires=['pyconfig'],
+      ext_modules = ext_modules,
      )
